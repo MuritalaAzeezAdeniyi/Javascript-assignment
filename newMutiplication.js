9bae1fafb59eb230da65feed6f066fe20e32818f
MuritalaAzeezAdeniyi <azeezmuritalaadeniyi@gmail.com>
2024-04-25 22:58:30 -0700

    adding file

---
+++ newMutiplication.js
@@ -0,0 +1,6 @@
+for(let number = 1; number <= 12; number1++){
+ 	for(let number = 1; number2 <= 12; number2++){
+ 	process.stdout.write(`${number2} * ${number1} = ${number2 * number1}\t`)
+	}
+   console.log()
+        }
